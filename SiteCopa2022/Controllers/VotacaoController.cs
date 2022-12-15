cef1dd6a406734b81c608e5684abab80f9cac2da
Renato Groffe <renato.groffe@yahoo.com.br>
2022-12-15 15:31:48 -0300

    .NET 7 + ASP.NET Core + MVC + Azure Event Hubs + Azure Application Insights (via Connection String) + Dockerfile + Enquete Copa do Mundo 2022 (Voto - versão 2)
    .NET 7 + ASP.NET Core + MVC + Azure Event Hubs + Azure Application Insights (via Connection String) + Dockerfile + Enquete Copa do Mundo 2022 (Voto - versão 2)

---
+++ SiteCopa2022/Controllers/VotacaoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteCopa2022.EventHubs;
+
+namespace SiteCopa2022.Controllers;
+
+public class VotacaoController : Controller
+{
+    private readonly ILogger<VotacaoController> _logger;
+    private readonly VotacaoProducer _producer;
+
+    public VotacaoController(ILogger<VotacaoController> logger,
+        VotacaoProducer producer)
+    {
+        _logger = logger;
+        _producer = producer;
+    }
+
+    public async Task<IActionResult> VotoBrasil()
+    {
+        return await ProcessarVoto("Brasil");
+    }
+
+    public async Task<IActionResult> VotoArgentina()
+    {
+        return await ProcessarVoto("Argentina");
+    }
+
+    public async Task<IActionResult> VotoFranca()
+    {
+        return await ProcessarVoto("Franca");
+    }
+
+    public async Task<IActionResult> VotoOutro()
+    {
+        return await ProcessarVoto("Outro");
+    }
+
+    private async Task<IActionResult> ProcessarVoto(string pais)
+    {
+        _logger.LogInformation($"Processando voto para o interesse: {pais}");
+        await _producer.Send(pais);
+        _logger.LogInformation($"Informações sobre o voto '{pais}' enviadas para o Azure Event Hubs!");
+
+        TempData["Voto"] = pais;
+        return RedirectToAction("Index", "Home", new { voto = pais });
+    }
+}
---
+++ SiteCopa2022/EventHubs/Voto.cs
@@ -0,0 +1,9 @@
+namespace SiteCopa2022.EventHubs;
+
+public class Voto
+{
+    public string? IdVoto { get; set; }
+    public string? Horario { get; set; }
+    public string? Producer { get; set; }
+    public string? Pais { get; set; }
+}
